a275e0b15e7053d61ad6ff669f4e77d4a291f4c0
SahilTech270 <YOUR_NOREPLY@users.noreply.github.com>
2026-04-26 15:00:28 +0530

    Initial Commit.

---
+++ src/test/java/SmartMedSutra/ApplicationTests.java
@@ -0,0 +1,13 @@
+package SmartMedSutra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
